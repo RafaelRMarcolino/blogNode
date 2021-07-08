d1bbd9520a1201a5c44f255e5c47b821c3f6e4b2
ozzy <ralfrr6@gmail.com>
2021-07-07 21:11:11 -0300

    flaskOK

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const admin = require("./routers/admin");
 const path = require("path");
 const mongoose = require('mongoose');
-
+const flash = require('connect-flash');
+const session = require('express-session')
 
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -20,6 +21,22 @@ mongoose.connect('mongodb://localhost:27017/blogt', {useNewUrlParser: true}).the
 }) 
 
 
+//session
+app.use(session({
+  secret: 'blogt',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+//flash
+app.use(flash());
+
+
+app.configure(function() {
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({ cookie: { maxAge: 60000 }}));
+  app.use(flash());
+});
 
 
 app.use(express.static(path.join(__dirname, 'public')));
