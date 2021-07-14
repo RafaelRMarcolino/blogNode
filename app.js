10e780eadc110016f2d63516bf688e4ef176cc80
ozzy <ralfrr6@gmail.com>
2021-07-13 21:13:18 -0300

    II

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.send("Deus e bom");
+    res.render('inicio.handlebars');
 });
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
